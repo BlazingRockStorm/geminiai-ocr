4caae22c9bf43b136d292433fa70bc34056c9bc0
BlazingRockStorm <rocker.gryphon@gmail.com>
2024-08-06 14:44:18 +0900

    init commit

---
+++ app.py
@@ -0,0 +1,41 @@
+import google.generativeai as genai
+from dotenv import load_dotenv
+load_dotenv()
+import os
+import PIL.Image
+
+genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
+image_path = "./content/example.png"
+
+json_schema = {
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "type": "object",
+  "properties": {
+    "address": {
+      "type": "string",
+    },
+    "name": {
+      "type": "string",
+    },
+    "post_code": {
+      "type": "string",
+    },
+  },
+  "required": ["address", "name", "post_code"]
+}
+
+img = PIL.Image.open(image_path)
+
+model = genai.GenerativeModel('gemini-1.5-flash-latest')
+
+completion = model.generate_content(["""\
+## 命令
+この画像に表示されている内容について回答してください。
+請求先の住所(address), 氏名(name), 郵便番号(post_code)を教えて下さい。
+
+## Output
+""",
+img]
+)
+
+print(completion.text)
